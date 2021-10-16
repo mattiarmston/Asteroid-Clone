08cb75cea45ad6b0eacff1742943b9d4a67d2646
Crus4d3 <91841476+Crus4d3@users.noreply.github.com>
2021-10-16 18:25:30 +0100

    Updated main file
    Added momentum based movement, better asteroid spawning, replay option (currently broken) and program no longer crashes after a game over.

--- __main__.py
+++ __main__.py
@@ -5,9 +5,9 @@ import random
 pygame.init()
 
 #Setting up window
-WIDTH, HEIGHT = 750, 750
+WIDTH, HEIGHT = 1000, 1000
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Top down shooter")
+pygame.display.set_caption("Asteroid clone")
 
 #Setting up images
 BackgroundImage = pygame.transform.scale(pygame.image.load(os.path.join("assets", "blankBG.png")), (WIDTH, HEIGHT))
@@ -17,13 +17,16 @@ asteroidImage = pygame.image.load(os.path.join("assets", "asteroidImage.png"))
 class Game():
     def __init__(self):
         self.run = True
-        self.FPS = 120
+        self.lost = False
+        self.FPS = 240
+        self.toDraw = []
         self.framesPassed = 0
         self.clock = pygame.time.Clock()
         self.score = 0
+        self.mainFont = pygame.font.SysFont("comicsans", 70)
     def drawFrame(self):
         WINDOW.blit(BackgroundImage, (0,0))
-        for item in toDraw:
+        for item in self.toDraw:
             WINDOW.blit(item.image, (item.x, item.y))
         pygame.display.update()
     def takeInputs(self):
@@ -31,68 +34,92 @@ class Game():
             if event.type == pygame.QUIT:
                 self.run = False
         self.keys = pygame.key.get_pressed()
+    def initGame(self):
+        self.player = Player(int(WIDTH/2), int(HEIGHT/2), 35, 35, 1, playerImage)
+        self.toDraw.append(self.player)
+        self.lost = False
     def main(self):
-        while self.run:
+        self.initGame()
+        while self.lost == False:
             self.clock.tick(self.FPS)
             #Used for spawning x asteroids per second. Need to find better solution.
             self.framesPassed += 1
             self.score += 1
             if self.framesPassed > self.FPS:
                 self.framesPassed = 0
-            masteroid.spawnAsteroid()
+            Asteroid.spawnAsteroid()
             self.drawFrame()
             self.takeInputs()
-            for item in toDraw:
+            for item in self.toDraw:
                 item.moveSelf()
-            player.hitAsteroid()
+            self.player.hitAsteroid()
+        scoreText = self.mainFont.render("Your score is {}".format(self.score), 1, (255, 255, 255))
+        WINDOW.blit(scoreText, (WIDTH/2 - scoreText.get_width()/2, HEIGHT/2 - scoreText.get_height()/2))
+        pygame.display.update()
+        time.sleep(2)
+        print("Wake up!")
+    def mainMenu(self):
+        while self.run:
+            self.main()
 
 class Player():
-    def __init__(self, x, y, width, height, speed, acceleration, image):
+    def __init__(self, x, y, width, height, acceleration, image):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.speed = speed
-        self.acceleration
+        self.speedY = 0
+        self.speedX = 0
+        self.acceleration = acceleration
         self.image = pygame.transform.scale(playerImage, (self.width, self.height))
         self.mask = pygame.mask.from_surface(self.image)
     def moveSelf(self):
-        if game.keys[pygame.K_w] and self.y - self.speed > 0:
-            self.y -= self.speed
-        if game.keys[pygame.K_s] and self.y + self.speed + self.height < HEIGHT:
-            self.y += self.speed
-        if game.keys[pygame.K_a] and self.x - self.speed > 0:
-            self.x -= self.speed
-        if game.keys[pygame.K_d] and self.x + self.speed + self.width < WIDTH:
-            self.x += self.speed
+        if game.keys[pygame.K_w]:
+            self.speedY -= self.acceleration
+        if game.keys[pygame.K_s]:
+            self.speedY += self.acceleration
+        if game.keys[pygame.K_a]:
+            self.speedX -= self.acceleration
+        if game.keys[pygame.K_d]:
+            self.speedX += self.acceleration
+        if self.y + self.speedY + self.height > HEIGHT:
+            self.y = HEIGHT - self.height
+            self.speedY = 0
+        elif self.y + self.speedY < 0:
+            self.y = 0
+            self.speedY = 0
+        else:
+            self.y += self.speedY
+        if self.x + self.speedX + self.width > WIDTH:
+            self.x = WIDTH - self.width
+            self.speedX = 0
+        elif self.x + self.speedX < 0:
+            self.x = 0
+            self.speedX = 0
+        else:
+            self.x += self.speedX
     def hitAsteroid(self):
-        for item in toDraw:
+        for item in game.toDraw:
             offsetX = item.x - self.x
             offsetY = item.y - self.y
             if self.mask.overlap(item.mask, (offsetX, offsetY)) != None:
                 if item.mask != self.mask:
-                    print("Your score is {}".format(game.score))
-                    game.run = False
+                    game.lost = True
 
 class Asteroid():
-    def __init__(self, width, height, speedX, speedY, image, masteroid):
+    def __init__(self, width, height, speedX, speedY, image):
         self.width = width
         self.height = height
         self.speedX = speedX
         self.speedY = speedY
         self.image = image
         self.mask = pygame.mask.from_surface(self.image)
-        self.masteroid = masteroid
-        if self.masteroid:
-            self.x = WIDTH * 2
-            self.y = HEIGHT * 2
-        else:
-            #Sets x and y position and ensures asteroids move onto screen
-            self.spawnLocation()
-    def spawnAsteroid(self):
+        #Sets x and y position and ensures asteroids move onto screen
+        self.spawnLocation()
+    def spawnAsteroid():
         if game.framesPassed % 60 == 0:
-            asteroid = Asteroid(50, 50, random.randint(-15, 15), random.randint(-15,15), asteroidImage, False)
-            toDraw.append(asteroid)
+            asteroid = Asteroid(50, 50, random.randint(-15, 15), random.randint(-15,15), asteroidImage)
+            game.toDraw.append(asteroid)
     def spawnLocation(self):
         #Decides whether asteroid spaws on left/right or top/bottom edge of the screen
         if random.randint(0,1) == 1:
@@ -100,32 +127,28 @@ class Asteroid():
             if random.randint(0,1) == 1:
                 #Asteroid spawns on left side of screen
                 self.x = 0 - self.width
-                self.y = random.randint(0, HEIGHT)
+                self.y = random.randint(int(HEIGHT/2), int(HEIGHT * 3/4))
                 self.speedX = random.randint(1, 15)
             else:
                 #Asteroid spawns on the right side of the screen
                 self.x = WIDTH + self.width
-                self.y = random.randint(0, HEIGHT)
+                self.y = random.randint(int(HEIGHT/2), int(HEIGHT * 3/4))
                 self.speedX = random.randint(-15, -1)
         else:
             #Asteroid spawns on top/bottom side of the screen
             if random.randint(0,1) == 1:
                 #Asteroid spawns on top side of the screen
-                self.x = random.randint(0, WIDTH)
+                self.x = random.randint(int(WIDTH/2), int(WIDTH * 3/4))
                 self.y = 0 - self.height
                 self.speedY = random.randint(1, 15)
             else:
                 #Asteroid spawns on bottom side of the screen
-                self.x = random.randint(0, WIDTH)
+                self.x = random.randint(int(WIDTH/2), int(WIDTH * 3/4))
                 self.y = HEIGHT + self.height
                 self.speedY = random.randint(-15, -1)
     def moveSelf(self):
         self.x += self.speedX
         self.y += self.speedY
         
-toDraw = []
-player = Player(int(WIDTH/2), int(HEIGHT/2), 35, 35, 10, 1, playerImage)
-masteroid = Asteroid(50, 50, 0, 0, asteroidImage, True)
-toDraw.append(player)
 game = Game()
-game.main()
+game.mainMenu()

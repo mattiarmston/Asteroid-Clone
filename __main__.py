ca549dbad35dfacbc1dea8046aede823266cf018
Matti Armston <matti.armston@gmail.com>
2024-06-20 17:52:00 +0100

    Removed unneeded files
    I have removed unneeded files from the repository, but kept local copies
    on my machine. This includes sound effects and images that are no longer
    used and an incomplete option to train a neural network to play the
    game. This feature should have been developed on a separate branch but I
    did not know enough about git to do that at the time. If I resume
    development of it that is what I shall do, but this is low priority.

--- __main__.py
+++ __main__.py
@@ -14,11 +14,6 @@ def parseArgs():
         action='store_true',
         help='output version information and exit'
     )
-    #parser.add_argument(
-    #    '--ai',
-    #    action='store_true',
-    #    help='uses neat to train ai to play'
-    #)
     return parser.parse_args()
 
 def main():
@@ -41,34 +36,16 @@ def loadGame(args):
         game = Game(scores)
         game.titleScreen()
 
-    def ai():
-        startNeat()
-
     def version():
-        print("Version {}".format('0.4.0'))
+        print("Version {}".format('0.5.0'))
         print("This project is still in active development")
-        print("Visit https://github.com/crus4d3/asteroid-clone for the latest version")
+        print("Visit https://github.com/mattiarmston/asteroid-clone for the latest version")
 
     if args.version:
         version()
         quit()
-    #if args.ai:
-    #    ai()
     else:
         player()
-
-def startNeat():
-    return
-    import neat
-    config = neat.config.Config(
-        neat.genome.DefaultGenome,
-        neat.reproduction.DefaultReproduction,
-        neat.species.DefaultSpeciesSet,
-        neat.stagnation.DefaultStagnation,
-        'neatConfig.txt'
-    )
-    population = neat.population.Population(config)
-    population.run(eval_genomes, 50)
 
 if __name__ == "__main__":
     main()
